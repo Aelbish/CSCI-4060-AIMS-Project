eedc9011a58b505a831f3f4e2286e89902d27388
sushantk10 <45109985+sushantk10@users.noreply.github.com>
2021-02-16 13:33:06 -0600

    Update MainActivity.java
    Turning off the visibility of Show coordinates button

--- app/src/main/java/com/example/aimsdispatcher/MainActivity.java
+++ app/src/main/java/com/example/aimsdispatcher/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -26,13 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
     private LocationListener locationListner;
-    String latitude ="";
-    String longitude ="";
+    String latitude = "";
+    String longitude = "";
     EditText city;
     TextView coordinates;
     RadioGroup group;
     RadioButton bt1, bt2;
-    Button btncoordinates,btnsavedata,btnshow;
+    Button btncoordinates, btnsavedata, btnshow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,37 +51,40 @@ public class MainActivity extends AppCompatActivity {
         group = (RadioGroup) findViewById(R.id.group);
         bt1 = (RadioButton) findViewById(R.id.bt1);
         bt2 = (RadioButton) findViewById(R.id.bt2);
-        btncoordinates =(Button) findViewById(R.id.btncoordinates);
+        btncoordinates = (Button) findViewById(R.id.btncoordinates);
         btnsavedata = (Button) findViewById(R.id.btnsavedata);
-        btnshow= (Button) findViewById(R.id.btnshow);
+        btnshow = (Button) findViewById(R.id.btnshow);
 
-        coordinates.setVisibility(View.GONE);
+//        coordinates.setVisibility(View.GONE);
+        btncoordinates.setVisibility(View.GONE);
 
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationListner = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
-                latitude=location.getLatitude()+"";
-                longitude=location.getLongitude()+"";
-                coordinates.setText("Latitude: "+location.getLatitude()+" Longitude: "+location.getLongitude());
+                latitude = location.getLatitude() + "";
+                longitude = location.getLongitude() + "";
+                coordinates.setText("Latitude: " + location.getLatitude() + " Longitude: " + location.getLongitude());
             }
 
             @Override
             public void onProviderDisabled(@NonNull String provider) {
-                Intent intent =new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivity(intent);
             }
         };
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET},1);
-            return;
-        }
-        else {
-            configureButton();
-        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 1);
+                return;
+            } else {
 
+                configureButton();
+            }
+
+        }
     }
     public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){
         switch(requestCode){
@@ -92,15 +96,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void configureButton() {
-        btncoordinates.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
-            @Override
-            public void onClick(View v) {
-                coordinates.setVisibility(View.VISIBLE);
-                locationManager.requestLocationUpdates("gps", 100, 100, locationListner);
-            }
-        });
+
+    @SuppressLint("MissingPermission")
+    private void configureButton() {
+
+//        coordinates.setVisibility(View.VISIBLE);
+        locationManager.requestLocationUpdates("gps", 100, 100, locationListner);
+
+//        btncoordinates.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
 
 
     }
