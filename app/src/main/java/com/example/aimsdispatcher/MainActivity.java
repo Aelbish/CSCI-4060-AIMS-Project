3d42a80d539403897c004bbd55109e84ed8eb1d8
sushantk10 <45109985+sushantk10@users.noreply.github.com>
2021-02-16 14:12:07 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/aimsdispatcher/MainActivity.java
+++ app/src/main/java/com/example/aimsdispatcher/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                     ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 1);
-                return;
+                configureButton();
             } else {
 
                 configureButton();
