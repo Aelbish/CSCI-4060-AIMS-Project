dc94118a08baedfdcf73babbba35d4d1d9638950
bijaygiri77 <bijaygiri771@gmail.com>
2021-03-15 22:08:22 -0500

    test for how to use restful api to get Json array.

--- app/src/main/java/csci4060/project/aimsmobileapp/MainActivity.java
+++ app/src/main/java/csci4060/project/aimsmobileapp/MainActivity.java
@@ -5,6 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
 
 //This is the Login Page
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AndroidNetworking.initialize(getApplicationContext());
+
+        AndroidNetworking.get("https://fierce-cove-29863.herokuapp.com/getAllUsers/{pageNumber}")
+                .addPathParameter("pageNumber", "0")
+                .addQueryParameter("limit", "3")
+                .addHeaders("token", "1234")
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        final String bla = response.toString();
+                        TextView test = (TextView) findViewById(R.id.textView);
+                        test.setText(bla);
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                    }
+                });
     }
 
     public void sendMessage(View view){
