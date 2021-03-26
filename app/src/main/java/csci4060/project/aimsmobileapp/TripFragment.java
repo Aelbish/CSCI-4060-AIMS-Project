578927587fad402b9b614949999d0290345e8dbe
Biebek Chamlagain <biebekchamlagain@gmail.com>
2021-03-26 16:06:50 -0500

    activity for trips information and recycler view was created

--- app/src/main/java/csci4060/project/aimsmobileapp/TripFragment.java
+++ app/src/main/java/csci4060/project/aimsmobileapp/TripFragment.java
@@ -14,6 +14,7 @@ public class TripFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_trips, container, false);
+        View view=inflater.inflate(R.layout.fragment_trips, container, false);
+        return view;
     }
 }
