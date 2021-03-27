650531dbd8e42a0a9ee83fb98c0884921ab5d242
Biebek Chamlagain <biebekchamlagain@gmail.com>
2021-03-27 03:26:37 -0500

    data send to another activity through adapter for individual trip

--- app/src/main/java/csci4060/project/aimsmobileapp/TripActivity.java
+++ app/src/main/java/csci4060/project/aimsmobileapp/TripActivity.java
@@ -1,14 +1,151 @@
 package csci4060.project.aimsmobileapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class TripActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class TripActivity extends AppCompatActivity {
+    TextView
+            txtTripId,
+            txtDriverCode,
+            txtDriverName,
+            txtTruckId,
+            txtTruckCode,
+            txtTruckDesc,
+            txtTrailerId,
+            txtTrailerCode,
+            txtTrailerDesc,
+            txtTripName,
+            txtTripDate,
+            txtDestinationCode,
+            txtAddress,
+            txtCity,
+            txtPostalCode,
+            txtProductId,
+            txtProductDesc,
+            txtRequestedQty,
+            txtUOM,
+            txtFill;
+
+    String
+            TripId,
+            DriverCode,
+            DriverName,
+            TruckId,
+            TruckCode,
+            TruckDesc,
+            TrailerId,
+            TrailerCode,
+            TrailerDesc,
+            TripName,
+            TripDate,
+            DestinationCode,
+            Address,
+            City,
+            PostalCode,
+            ProductId,
+            ProductDesc,
+            RequestedQty,
+            UOM,
+            Fill;
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
+
+        txtTripId = findViewById(R.id.TripId);
+        txtDriverCode = findViewById(R.id.DriverCode);
+        txtDriverName = findViewById(R.id.DriverName);
+        txtTruckId = findViewById(R.id.TruckId);
+        txtTruckCode = findViewById(R.id.TruckCode);
+        txtTruckDesc = findViewById(R.id.TruckDesc);
+        txtTrailerId = findViewById(R.id.TrailerId);
+        txtTrailerCode = findViewById(R.id.TrailerCode);
+        txtTrailerDesc = findViewById(R.id.TrailerDesc);
+        txtTripName = findViewById(R.id.TripName);
+        txtTripDate = findViewById(R.id.TripDate);
+        txtDestinationCode = findViewById(R.id.DestinationCode);
+        txtAddress = findViewById(R.id.Address1);
+        txtCity = findViewById(R.id.City);
+        txtPostalCode = findViewById(R.id.PostalCode);
+        txtProductId = findViewById(R.id.ProductId);
+        txtProductDesc = findViewById(R.id.ProductDesc);
+        txtRequestedQty = findViewById(R.id.RequestedQty);
+        txtUOM = findViewById(R.id.UOM);
+        txtFill = findViewById(R.id.Fill);
+
+        btnStart = findViewById(R.id.btn_start_trip);
+        btnStart.setOnClickListener(this);
+
+        getSingleTripDetails();
+
+    }
+
+    private void getSingleTripDetails() {
+//        Getting and storing each value of specific trip list in the following variables
+        Bundle bundle = getIntent().getExtras();
+
+        TripId = bundle.getString("TripId");
+        DriverCode = bundle.getString("DriverCode");
+        DriverName = bundle.getString("DriverName");
+        TruckId = bundle.getString("TruckId");
+        TruckCode = bundle.getString("TruckCode");
+        TruckDesc = bundle.getString("TruckDesc");
+        TrailerId = bundle.getString("TrailerId");
+        TrailerCode = bundle.getString("TrailerCode");
+        TrailerDesc = bundle.getString("TrailerDesc");
+        TripName = bundle.getString("TripName");
+        TripDate = bundle.getString("TripDate");
+        DestinationCode = bundle.getString("DestinationCode");
+        Address = bundle.getString("Address");
+        City = bundle.getString("City");
+        PostalCode = bundle.getString("PostalCode");
+        ProductId = bundle.getString("ProductId");
+        ProductDesc = bundle.getString("ProductDesc");
+        RequestedQty = bundle.getString("RequestedQty");
+        UOM = bundle.getString("UOM");
+        UOM = bundle.getString("UOM");
+        Fill = bundle.getString("Fill");
+
+
+//       Rendering the subscribed data from recycler view into text views
+        txtTripId.setText("Trip id: " + TripId);
+        txtDriverCode.setText("Driver code: " + DriverCode);
+        txtDriverName.setText("Driver name: " + DriverName);
+        txtTruckId.setText("Truck id" + TruckId);
+        txtTruckCode.setText("Truck code" + TruckCode);
+        txtTruckDesc.setText("Truck Desc: " + TruckDesc);
+        txtTrailerId.setText("Trailer id" + TrailerId);
+        txtTrailerCode.setText("Trailer code " + TrailerCode);
+        txtTrailerDesc.setText("Trailer Desc" + TrailerDesc);
+        txtTripName.setText("Trip name " + TripName);
+        txtTripDate.setText("Trip date " + TripDate);
+        txtDestinationCode.setText("Destination code " + DestinationCode);
+        txtAddress.setText("Address1" + Address);
+        txtCity.setText("City " + City);
+        txtPostalCode.setText("Postal code " + PostalCode);
+        txtProductId.setText("Product id" + ProductId);
+        txtProductDesc.setText("Product desc" + ProductDesc);
+        txtRequestedQty.setText("Request qunatity: " + RequestedQty);
+        txtUOM.setText("UOM " + UOM);
+        txtFill.setText("Fill " + Fill);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_start_trip) {
+//            Send put request update to AIMS api
+            Toast.makeText(this, "Send udpate to aims", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/csci4060/project/aimsmobileapp/TripListAdapter.java
+++ app/src/main/java/csci4060/project/aimsmobileapp/TripListAdapter.java
@@ -2,9 +2,11 @@ package csci4060.project.aimsmobileapp;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -39,9 +41,39 @@ public class TripListAdapter extends RecyclerView.Adapter<TripListAdapter.ItemsV
         final TripInfoModel tripInfoModel = tripInfoModelList.get(i);
 
 
-        itemsViewHolder.txtTripId.setText("Trip id: "+tripInfoModel.getTripId());
-        itemsViewHolder.txtTripName.setText("Trip name: "+tripInfoModel.getTripName());
+        itemsViewHolder.txtTripId.setText("Trip id: " + tripInfoModel.getTripId());
+        itemsViewHolder.txtTripName.setText("Trip name: " + tripInfoModel.getTripName());
+        itemsViewHolder.btnSummary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.btn_summary) {
+                    Intent intent = new Intent(mcontext, TripActivity.class);
+                    intent.putExtra("TripId", String.valueOf(tripInfoModel.getTripId()));
+                    intent.putExtra("DriverCode", tripInfoModel.getDriverCode());
+                    intent.putExtra("DriverName", tripInfoModel.getDriverName());
+                    intent.putExtra("TruckId", tripInfoModel.getTruckId());
+                    intent.putExtra("TruckCode", tripInfoModel.getTruckCode());
+                    intent.putExtra("TruckDesc", tripInfoModel.getTruckDesc());
+                    intent.putExtra("TrailerId", tripInfoModel.getTrailerId());
+                    intent.putExtra("TrailerCode", tripInfoModel.getTrailerCode());
+                    intent.putExtra("TrailerDesc", tripInfoModel.getTrailerDesc());
+                    intent.putExtra("TripName", tripInfoModel.getTripName());
+                    intent.putExtra("TripDate", tripInfoModel.getTripDate());
+                    intent.putExtra("DestinationCode", tripInfoModel.getDestinationCode());
+                    intent.putExtra("Address", tripInfoModel.getAddress());
+                    intent.putExtra("City", tripInfoModel.getCity());
+                    intent.putExtra("PostalCode", tripInfoModel.getPostalCode());
+                    intent.putExtra("ProductId", tripInfoModel.getProductId());
+                    intent.putExtra("ProductDesc", tripInfoModel.getProductDesc());
+                    intent.putExtra("RequestedQty", tripInfoModel.getRequestedQty());
+                    intent.putExtra("UOM", tripInfoModel.getUOM());
+                    intent.putExtra("Fill", tripInfoModel.getFill());
 
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    mcontext.startActivity(intent);
+                }
+            }
+        });
 
     }
 
@@ -54,6 +86,7 @@ public class TripListAdapter extends RecyclerView.Adapter<TripListAdapter.ItemsV
     public class ItemsViewHolder extends RecyclerView.ViewHolder {
 
         public TextView txtTripId, txtTripName;
+        public Button btnSummary;
 
         public ItemsViewHolder(@NonNull View itemView) {
 
@@ -61,6 +94,9 @@ public class TripListAdapter extends RecyclerView.Adapter<TripListAdapter.ItemsV
 
             txtTripId = itemView.findViewById(R.id.txtTripId);
             txtTripName = itemView.findViewById(R.id.txtTripName);
+            btnSummary = itemView.findViewById(R.id.btn_summary);
+
+
         }
 
     }
