807577ce252c581bb6141306bc72db36b91117a2
bijaygiri77 <bijaygiri771@gmail.com>
2021-03-26 22:31:32 -0500

    This version of code imports the data using Rest api call implementing volley and displays it in textView in login page.

--- app/src/main/java/csci4060/project/aimsmobileapp/MainActivity.java
+++ app/src/main/java/csci4060/project/aimsmobileapp/MainActivity.java
@@ -7,12 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.androidnetworking.AndroidNetworking;
-import com.androidnetworking.common.Priority;
-import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
+import org.json.JSONObject;
 
 //This is the Login Page
 public class MainActivity extends AppCompatActivity {
@@ -21,27 +28,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AndroidNetworking.initialize(getApplicationContext());
 
-        AndroidNetworking.get("https://fierce-cove-29863.herokuapp.com/getAllUsers/{pageNumber}")
-                .addPathParameter("pageNumber", "0")
-                .addQueryParameter("limit", "3")
-                .addHeaders("token", "1234")
-                .setTag("test")
-                .setPriority(Priority.LOW)
-                .build()
-                .getAsJSONArray(new JSONArrayRequestListener() {
+        TextView displayResult = findViewById(R.id.textView);
+
+        RequestQueue requestQueue;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        requestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        requestQueue.start();
+
+        //making the Rest api call
+        String url = "https://api.appery.io/rest/1/apiexpress/api/DispatcherMobileApp/GetTripListDetailByDriver/D1?apiKey=f20f8b25-b149-481c-9d2c-41aeb76246ef";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
                     @Override
-                    public void onResponse(JSONArray response) {
-                        final String bla = response.toString();
-                        TextView test = (TextView) findViewById(R.id.textView);
-                        test.setText(bla);
+                    public void onResponse(JSONObject response) {
+                        displayResult.setText(response.toString());
                     }
+                }, new Response.ErrorListener() {
+
                     @Override
-                    public void onError(ANError error) {
-                        // handle error
+                    public void onErrorResponse(VolleyError error) {
                     }
                 });
+
+        requestQueue.add(jsonObjectRequest);
     }
 
     public void sendMessage(View view){
