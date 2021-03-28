b353e48148295a1c9ea6edafd4d7cebb73681b48
Aelbish Shrestha <aelbish@gmail.com>
2021-03-27 23:39:47 -0500

    Added driver input feature

---
+++ app/src/main/java/csci4060/project/aimsmobileapp/DriverInputSourceActivity.java
@@ -0,0 +1,23 @@
+package csci4060.project.aimsmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class DriverInputSourceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_input_source);
+
+//        Spinner mySpinner = (Spinner) findViewById(R.id.);
+//
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(DriverInputSourceActivity.this,
+//                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.productNames));
+//        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        mySpinner.setAdapter(myAdapter);
+    }
+}
