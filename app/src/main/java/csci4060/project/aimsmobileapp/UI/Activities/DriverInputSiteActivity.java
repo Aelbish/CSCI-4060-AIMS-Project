7dc69f06a875ad8393c480870cee8994ff7b83da
Aelbish Shrestha <aelbish@gmail.com>
2021-04-10 10:37:08 -0500

    Updated Driver Input For Source Activity. Added Date and Time Picker for the driver to input date and time. Added "BOL Scan" feature to capture image of the Bill of Lading. The "BOL picture" will also be loaded on the input screen.

---
+++ app/src/main/java/csci4060/project/aimsmobileapp/UI/Activities/DriverInputSiteActivity.java
@@ -0,0 +1,16 @@
+package csci4060.project.aimsmobileapp.UI.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import csci4060.project.aimsmobileapp.R;
+
+public class DriverInputSiteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_input_site);
+    }
+}
