ae452d474f7c525d91f19826e2b6a0a206820ad5
Biebek Chamlagain <biebekchamlagain@gmail.com>
2021-04-12 06:57:02 -0500

    summary activity redesigned

--- app/src/main/java/csci4060/project/aimsmobileapp/UI/Activities/TripActivity.java
+++ app/src/main/java/csci4060/project/aimsmobileapp/UI/Activities/TripActivity.java
@@ -28,21 +28,14 @@ public class TripActivity extends AppCompatActivity implements View.OnClickListe
             txtTripId,
             txtDriverCode,
             txtDriverName,
-            txtTruckId,
-            txtTruckCode,
             txtTruckDesc,
-            txtTrailerId,
-            txtTrailerCode,
             txtTrailerDesc,
             txtTripName,
             txtTripDate,
-            txtDestinationCode,
             txtDestinationName,
             txtAddress,
             txtCity,
             txtPostalCode,
-            txtProductId,
-            txtProductCode,
             txtProductDesc,
             txtRequestedQty,
             txtUOM,
@@ -52,21 +45,14 @@ public class TripActivity extends AppCompatActivity implements View.OnClickListe
             TripId,
             DriverCode,
             DriverName,
-            TruckId,
-            TruckCode,
             TruckDesc,
-            TrailerId,
-            TrailerCode,
             TrailerDesc,
             TripName,
             TripDate,
-            DestinationCode,
             DestinationName,
             Address,
             City,
             PostalCode,
-            ProductId,
-            ProductCode,
             ProductDesc,
             RequestedQty,
             UOM,
@@ -83,21 +69,14 @@ public class TripActivity extends AppCompatActivity implements View.OnClickListe
         txtTripId = findViewById(R.id.TripId);
         txtDriverCode = findViewById(R.id.DriverCode);
         txtDriverName = findViewById(R.id.DriverName);
-        txtTruckId = findViewById(R.id.TruckId);
-        txtTruckCode = findViewById(R.id.TruckCode);
         txtTruckDesc = findViewById(R.id.TruckDesc);
-        txtTrailerId = findViewById(R.id.TrailerId);
-        txtTrailerCode = findViewById(R.id.TrailerCode);
         txtTrailerDesc = findViewById(R.id.TrailerDesc);
         txtTripName = findViewById(R.id.TripName);
         txtTripDate = findViewById(R.id.TripDate);
-        txtDestinationCode = findViewById(R.id.DestinationCode);
         txtDestinationName = findViewById(R.id.DestinationName);
         txtAddress = findViewById(R.id.Address1);
         txtCity = findViewById(R.id.City);
         txtPostalCode = findViewById(R.id.PostalCode);
-        txtProductId = findViewById(R.id.ProductId);
-        txtProductCode = findViewById(R.id.ProductCode);
         txtProductDesc = findViewById(R.id.ProductDesc);
         txtRequestedQty = findViewById(R.id.RequestedQty);
         txtUOM = findViewById(R.id.UOM);
@@ -121,21 +100,14 @@ public class TripActivity extends AppCompatActivity implements View.OnClickListe
         TripId = bundle.getString("TripId");
         DriverCode = bundle.getString("DriverCode");
         DriverName = bundle.getString("DriverName");
-        TruckId = bundle.getString("TruckId");
-        TruckCode = bundle.getString("TruckCode");
         TruckDesc = bundle.getString("TruckDesc");
-        TrailerId = bundle.getString("TrailerId");
-        TrailerCode = bundle.getString("TrailerCode");
         TrailerDesc = bundle.getString("TrailerDesc");
         TripName = bundle.getString("TripName");
         TripDate = bundle.getString("TripDate");
-        DestinationCode = bundle.getString("DestinationCode");
         DestinationName = bundle.getString("DestinationName");
         Address = bundle.getString("Address");
         City = bundle.getString("City");
         PostalCode = bundle.getString("PostalCode");
-        ProductId = bundle.getString("ProductId");
-        ProductCode = bundle.getString("ProductCode");
         ProductDesc = bundle.getString("ProductDesc");
         RequestedQty = bundle.getString("RequestedQty");
         UOM = bundle.getString("UOM");
@@ -144,28 +116,21 @@ public class TripActivity extends AppCompatActivity implements View.OnClickListe
 
 
 //       Rendering the subscribed data from recycler view into text views
-        txtTripId.setText("Trip id: " + TripId);
-        txtDriverCode.setText("Driver code: " + DriverCode);
-        txtDriverName.setText("Driver name: " + DriverName);
-        txtTruckId.setText("Truck id: " + TruckId);
-        txtTruckCode.setText("Truck code: " + TruckCode);
-        txtTruckDesc.setText("Truck Desc: " + TruckDesc);
-        txtTrailerId.setText("Trailer id: " + TrailerId);
-        txtTrailerCode.setText("Trailer code: " + TrailerCode);
-        txtTrailerDesc.setText("Trailer Desc: " + TrailerDesc);
-        txtTripName.setText("Trip name: " + TripName);
-        txtTripDate.setText("Trip date: " + TripDate);
-        txtDestinationCode.setText("Destination code: " + DestinationCode);
-        txtDestinationName.setText("Destination name: " + DestinationName);
-        txtAddress.setText("Address1: " + Address);
-        txtCity.setText("City: " + City);
-        txtPostalCode.setText("Postal code: " + PostalCode);
-        txtProductId.setText("Product id: " + ProductId);
-        txtProductCode.setText("Product code: " + ProductCode);
-        txtProductDesc.setText("Product desc: " + ProductDesc);
-        txtRequestedQty.setText("Request qunatity: " + RequestedQty);
-        txtUOM.setText("UOM: " + UOM);
-        txtFill.setText("Fill: " + Fill);
+        txtTripId.setText("TRIP ID: "+ TripId);
+        txtDriverCode.setText(DriverCode);
+        txtDriverName.setText(DriverName);
+        txtTruckDesc.setText(TruckDesc);
+        txtTrailerDesc.setText(TrailerDesc);
+        txtTripName.setText(TripName);
+        txtTripDate.setText(TripDate);
+        txtDestinationName.setText(DestinationName);
+        txtAddress.setText(Address);
+        txtCity.setText(City);
+        txtPostalCode.setText(PostalCode);
+        txtProductDesc.setText(ProductDesc);
+        txtRequestedQty.setText(RequestedQty);
+        txtUOM.setText(UOM);
+        txtFill.setText(Fill);
 
 
     }
