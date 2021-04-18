6fe11f8190bbd4d2d9a27951e4274af20e110b96
sushantk10 <45109985+sushantk10@users.noreply.github.com>
2021-04-18 01:49:21 -0500

    Just have a here map view for right now

--- app/src/main/java/csci4060/project/aimsmobileapp/UI/Fragments/RouteFragment.java
+++ app/src/main/java/csci4060/project/aimsmobileapp/UI/Fragments/RouteFragment.java
@@ -1,21 +1,112 @@
 package csci4060.project.aimsmobileapp.UI.Fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+
+import com.here.sdk.core.GeoCoordinates;
+import com.here.sdk.mapview.MapError;
+import com.here.sdk.mapview.MapScene;
+import com.here.sdk.mapview.MapScheme;
+import com.here.sdk.mapview.MapView;
+
+import java.util.Map;
+
 import csci4060.project.aimsmobileapp.R;
+import csci4060.project.aimsmobileapp.UI.Activities.MainActivity;
+import csci4060.project.aimsmobileapp.UI.Activities.MainScreenActivity;
+
+import static android.content.ContentValues.TAG;
+import static android.content.Context.LOCATION_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
 
 //This is route screen
 public class RouteFragment extends Fragment {
+
+    private MapView mapView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_route, container, false);
     }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        // Get a MapView instance from the layout.
+        mapView = getView().findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.setOnReadyListener(new MapView.OnReadyListener() {
+            @Override
+            public void onMapViewReady() {
+                // This will be called each time after this activity is resumed.
+                // It will not be called before the first map scene was loaded.
+                // Any code that requires map data may not work as expected beforehand.
+                Log.d(TAG, "HERE Rendering Engine attached.");
+            }
+        });
+
+
+        loadMapScene();
+    }
+
+    private void loadMapScene() {
+
+        // Load a scene from the HERE SDK to render the map with a map scheme.
+        mapView.getMapScene().loadScene(MapScheme.NORMAL_DAY, new MapScene.LoadSceneCallback() {
+            @Override
+            public void onLoadScene(@Nullable MapError mapError) {
+                if (mapError == null) {
+                    double distanceInMeters = 1000 * 10;
+                    mapView.getCamera().lookAt(
+                            new GeoCoordinates(32.5267,-92.0732), distanceInMeters);
+                } else {
+                    Log.d(TAG, "Loading map failed: mapError: " + mapError.name());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+
 }
