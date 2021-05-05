cc675e65cb4eea474afc260b1c23b544958219f3
cstrickland98 <cstrickland98@bellsouth.net>
2021-05-04 22:10:24 -0500

    Added null location check before calling get latitude and get longitude to prevent null reference crash.

--- app/src/main/java/csci4060/project/aimsmobileapp/UI/Fragments/navigation/RouteFragment.java
+++ app/src/main/java/csci4060/project/aimsmobileapp/UI/Fragments/navigation/RouteFragment.java
@@ -738,8 +738,10 @@ public class RouteFragment<afChangeListener> extends Fragment {
         }
         LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        longitude = location.getLongitude();
-        latitude = location.getLatitude();
+        if(location != null) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        }
         return true;
     }
 
